3d75d2cbdcc93ad6ef0d249dd588561d9346c89e
Hong <151532167+UChangh@users.noreply.github.com>
2023-12-01 11:46:20 +0900

    체크인 체크아웃 형식대로 입력하지 않을 시 에러발생(IndexOutofBounds) 선택한 방 번호 재입력시 중복예약 가능한 문제 존재 체크인, 체크아웃 함수 위치 이동 -> CheckDate

--- app/src/main/java/com/example/ssshotelbookingactivity/CheckDate.kt
+++ app/src/main/java/com/example/ssshotelbookingactivity/CheckDate.kt
@@ -13,3 +13,44 @@ open class CheckDate {
         return check.format(dateFormat)
     }
 }
+
+fun today(): String {
+    var today = LocalDate.now()
+    var dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd")
+    return today.format(dateFormat)
+}
+
+fun checkin(chkin:String):String {
+    var chkin = chkin
+    while(true) {
+        when {
+            chkin < today() -> {
+                println("오늘 날짜보다 이전 날짜는 선택하실 수 없습니다.")
+                println("날짜를 다시 입력해 주세요.")
+                chkin = CheckDate().check(input())
+            }
+            chkin >= today() -> {
+                println("체크인 날짜를 선택하셨습니다.")
+                return chkin
+                break
+            }
+        }
+    }
+}
+fun checkout(chkin:String, chkout:String):String {
+    var chkout = chkout
+    while(true) {
+        when {
+            chkin > chkout -> {
+                println("체크인 날짜보다 이전 날짜는 선택하실 수 없습니다.")
+                println("날짜를 다시 입력해 주세요.")
+                chkout = CheckDate().check(input())
+            }
+            chkout > chkin -> {     // chkin에서 today 검증을 하기 때문에 chkout에서 todayd검증은 하지 않음
+                println("체크아웃 날짜를 선택하셨습니다.")
+                return chkin
+                break
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/ssshotelbookingactivity/HotelBooking.kt
+++ app/src/main/java/com/example/ssshotelbookingactivity/HotelBooking.kt
@@ -10,6 +10,7 @@ data class Settings(
     var checkout: String
 )
 
+fun input() = readln()
 fun main() {
     var person = Settings("Admin", 100, "1900/01/01", "2100/12/31")
     while (true) {
@@ -32,8 +33,6 @@ fun main() {
         }
     }
 }
-
-fun input() = readln()
 
 fun roomno(room:Int):Int {
     var room = room
@@ -58,44 +57,4 @@ fun roomno(room:Int):Int {
         }
     }
     return room
-}
-fun today(): String {
-    var today = LocalDate.now()
-    var dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd")
-    return today.format(dateFormat)
-}
-fun checkin(chkin:String):String {
-    var chkin = chkin
-    while(true) {
-        when {
-            chkin < today() -> {
-                println("오늘 날짜보다 이전 날짜는 선택하실 수 없습니다.")
-                println("날짜를 다시 입력해 주세요.")
-                chkin = CheckDate().check(input())
-            }
-            chkin >= today() -> {
-                println("체크인 날짜를 선택하셨습니다.")
-                return chkin
-                break
-            }
-        }
-    }
-}
-
-fun checkout(chkin:String, chkout:String):String {
-    var chkout = chkout
-    while(true) {
-        when {
-            chkin > chkout -> {
-                println("체크인 날짜보다 이전 날짜는 선택하실 수 없습니다.")
-                println("날짜를 다시 입력해 주세요.")
-                chkout = CheckDate().check(input())
-            }
-            chkout > chkin -> {     // chkin에서 today 검증을 하기 때문에 chkout에서 todayd검증은 하지 않음
-                println("체크아웃 날짜를 선택하셨습니다.")
-                return chkin
-                break
-            }
-        }
-    }
 }
